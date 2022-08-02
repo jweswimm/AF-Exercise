7d47c268dcecdf4c2779bee6bccd3cbf4ce2bf7f
Joseph Wimmergren <105512186+jweswimm@users.noreply.github.com>
2022-08-02 11:14:20 -0400

    Added variables & state init to namespace
    Added the necessary libraries for curand, and initialization variables and kernel to the cuda namespace. 
    Fixed the gpuerrchk function to give us some error messages to help with debugging

--- common/common.h
+++ common/common.h
@@ -6,20 +6,24 @@
  * The complete license agreement can be obtained at:
  * http://arrayfire.com/licenses/BSD-3-Clause
  ********************************************************/
+#pragma once
 
 #include <stdio.h>
+#include <curand.h>
+#include <curand_kernel.h>
+#include <iostream>
+#include "cuda_runtime.h"
 
 #define PI 3.1415926535897931f
 
-//Add some error testing for cuda
 #define gpuErrchk(ans) { gpuAssert((ans), __FILE__, __LINE__); }
-inline void gpuAssert(cudaError_t code, const char* file, int line, bool abort = true)
+inline void gpuAssert(cudaError_t code, const char *file, int line, bool abort=true)
 {
-	if (code != cudaSuccess)
-	{
-		fprintf(stderr, "GPUassert: %s %s %d\n", cudaGetErrorString(code), file, line);
-		if (abort) exit(code);
-	}
+   if (code != cudaSuccess)
+   {
+      fprintf(stderr,"GPUassert: %s %s %d\n", cudaGetErrorString(code), file, line);
+      if (abort) exit(code);
+   }
 }
 
 // generate millions of random samples
@@ -32,9 +36,19 @@ namespace cuda
 #endif
 {
 
-void pi_init();
-double pi();
-void pi_reset();
+	//Variables
+	static unsigned int seed; //seed for curand, can be the same for each thread
+	static unsigned int count; //number of values distance < 1 away from center
+	static unsigned int block_size; //How many threads per block
+	static unsigned int grid_size; //How many blocks per grid
+
+	//CUDA kernels
+	__global__ void state_init(unsigned int sseed, unsigned int ssamples, curandState_t* sstates);
+
+	//Functions
+	void pi_init();
+	double pi();
+	void pi_reset();
 
 }
 
