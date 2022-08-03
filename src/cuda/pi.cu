72dcddf4cef642740e7fcb917a1a89a60927d1e5
Joseph Wimmergren <105512186+jweswimm@users.noreply.github.com>
2022-08-03 13:38:25 -0400

    Fixed some formatting
    Fixed formatting, wrote better comments

--- src/cuda/pi.cu
+++ src/cuda/pi.cu
@@ -9,37 +9,23 @@
 
 #include <common.h>
 
-
 namespace cuda
 {
-
-    __device__ double d_atomicAdd(double* address, double val)
-    {
-        unsigned long long int* address_as_ull = (unsigned long long int*)address;
-        unsigned long long int old = *address_as_ull, assumed;
-        do {
-            assumed = old;
-            old = atomicCAS(address_as_ull, assumed,
-                 __double_as_longlong(val + __longlong_as_double(assumed)));
-        } while (assumed != old);
-    return __longlong_as_double(old);
-    }
-
-
 
     //Initialize random states for curand call later
     __global__ void random_init(unsigned int seed, int samples, curandState* states) {
         for (int idx = blockIdx.x * blockDim.x + threadIdx.x; idx < samples; idx += gridDim.x * blockDim.x) {
+
             curand_init(seed, idx, 0, &states[idx]);
+
         }
 
     }
 
-
-
     //Initialize necessary variables, create seed, and allocate space on the device
     void pi_init_v1()
     {
+
         //Create seed--this can be the same for each thread, see https://ianfinlayson.net/class/cpsc425/notes/cuda-random
         seed = time(0);
 
@@ -49,11 +35,10 @@ namespace cuda
         grid_size = ceil(double(samples) / double(block_size)); //round up (block_size*grid_size = samples)
 
         //Allocated space on the device for the number of samples we need
-        cudaMalloc((void**)&d_states, block_size * grid_size * sizeof(curandState));
+        cudaMalloc((void**)&d_states, samples * sizeof(curandState));
 
         //Call curand_init kernel
         random_init << <grid_size, block_size >> > (seed, samples, d_states);
-
 
     }
 
@@ -61,8 +46,6 @@ namespace cuda
     //kernel for Monte Carlo estimation of pi
     __global__ void est_pi(int* count, unsigned int seed, int samples, curandState* states) {
         for (int idx = blockIdx.x * blockDim.x + threadIdx.x; idx < samples; idx += gridDim.x * blockDim.x) {
-
-//            curand_init(seed, idx, 0, &states[idx]);
 
             //Note: uniform distribution excludes 0 and includes 1
             //We may want a real distribution instead
@@ -92,9 +75,9 @@ namespace cuda
         In 2D, we overlay a unit circle on a unit square. The area of the
         unit circle is \pi r^2 and the area of the unit square is 4 r^2.
         If we randomly generate x and y coordinates for points,
-        the ratio of points inside the unit circle to points outside
+        the fraction of points inside the unit circle to points outside
         the unit circle and inside the unit square will be an excellent
-        approximation of pi.
+        approximation of pi, as long as all coordinates are equally likely.
         I.e. (inside circle)/(outside circle) = (\pi r^2) / (4 r^2) = \pi / 4
         <=> 4*(inside circle)/(outside circle) = \pi
         Admittedly, this random number generation only generates positive numbers,
@@ -107,6 +90,12 @@ namespace cuda
 
         //Initialize count to 0
         thrust::device_vector<int> count(1, 0);
+        //It would be faster (negligibly so, I think) to not use thrust here
+        //However, because we have to continuously reset the value of count,
+        //as af::timeit() calls the function over and over again, I thought
+        //it's better to just use thrust to do this instead of creating
+        //a pointer that points to 0, then cudamemcpy'ing the host pointer
+        // to the device pointer
 
         //Call kernel
         est_pi << <grid_size, block_size >> > (thrust::raw_pointer_cast(count.data()), seed, samples, d_states);
@@ -118,41 +107,28 @@ namespace cuda
 
     void pi_reset_v1()
     {
-        /*
-          TODO This function should contain the clean up. You should add
-          memory deallocation etc here.
-        */
-
         //Free d_states, as it was on the device
         cudaFree(d_states);
-
-
         //Don't need to free thrust device vector count
     }
-    __device__ double fact(int n)
-    {
-        if (n<2)
-            return 1;
 
-        return n*fact(n - 1);
-    }
 
-    __global__ void chudnovsky(int N, double* m, double* l, double* x, double* sum) {
+    //---Pi V2---//
+    //Cute example of some quick calculation to estimate pi
+    //using Chudnovsky algorithm
+
+    //Kernel for the chudnovsky algorithm to calculate pi
+    __global__ void chudnovsky(int N, double* sum) {
         for (int idx = blockIdx.x * blockDim.x + threadIdx.x;
             idx < N; idx += gridDim.x * blockDim.x) {
 
-//            l[idx] = l[idx - 1] + 163*127*19 * 11 * 9 * 2;
- //           x[idx] = x[idx - 1] * 640320*640320*640320;
-  //          m[idx] = m[idx - 1] * (((12*idx - 2) * (12*idx -6) * (12*idx - 10))/(idx*idx*idx));
-            double M = (fact(6 * idx))/(fact(3*idx)*pow(fact(idx),3));
-            double L = 54514013 * idx + 13591409;
-            double X = pow(-262537412640768000, idx);
+            //Calculate constituent parts of the sum
+            long long int M = (fact(6 * idx))/(fact(3*idx)*pow(fact(idx),3));
+            long long int L = 54514013 * idx + 13591409;
+            long long int X = pow(-262537412640768000, idx);
 
-            //Calculate individual components of the sum
-//            components[idx] = ;
-
+                //Calculate sum and avoid race conditions
                 d_atomicAdd((sum), (M * L) / X);
-
 
         }
 
@@ -162,48 +138,84 @@ namespace cuda
         //Calculate C
         C = 426880 * sqrt(10005);
 
-        N = 4;
+        //How many elements to sum
+        N = 16;
 
-        //Allocate space for the sum
-        //Each thread will be one value of the sum
-        thrust::device_vector<double> dum(N, 0);
-        sum_components = dum;
-        m = dum;
-        m[0] = 1;
-        l = dum;
-        l[0] = 13591409;
-        x = dum;
-        x[0] = 1;
-
+        //Set kernel init variables
         block_size2 = 16;
         grid_size2 = ceil(float(N) / float(block_size));
 
-
     }
 
-
-
-
-
     double pi_v2() {
+        /*
+        This algorithm is based on Ramanujan's infinite series calculation of pi
+        See https://en.wikipedia.org/wiki/List_of_formulae_involving_%CF%80#Infinite_series
+        See https://en.wikipedia.org/wiki/Chudnovsky_algorithm
+        See Chudnovsky, David; Chudnovsky, Gregory (1988)
+
+        Bottlenecked by storage space (large computations for factorials)
+        Might have to use hexadecimal for huge computations
+        See this interesting blog http://www.karrels.org/pi/ where they talk about
+        dealing with huge numbers
+        See this interesting software http://www.numberworld.org/y-cruncher/
+        */
 
 
+        //Reset sum to 0
         thrust::device_vector<double> sum(1, 0.0);
+        //It would be faster (negligibly so, I think) to not use thrust here
+        //However, because we have to continuously reset the value of count,
+        //as af::timeit() calls the function over and over again, I thought
+        //it's better to just use thrust to do this instead of creating
+        //a pointer that points to 0, then cudamemcpy'ing the host pointer
+        // to the device pointer
 
-        chudnovsky << < grid_size2, block_size2 >> > (N, thrust::raw_pointer_cast(m.data()),
-            thrust::raw_pointer_cast(l.data()), thrust::raw_pointer_cast(x.data()),
-                thrust::raw_pointer_cast(sum.data()));
-
-
-//        std::cout << C * (1 / sum[0]);
-//        std::cout << C * (1/sum[0]) << std::endl;
+        //Call kernel
+        chudnovsky << < grid_size2, block_size2 >> > (N, thrust::raw_pointer_cast(sum.data()));
 
         return C * (1/sum[0]);
+
     }
 
     void pi_reset_v2() {
-
+        //No cleanup required
+        //Thrust deallocates any associated storage when it goes out of scope
     }
+
+
+    //--Helper Functions--//
+
+    //Cute, slow, factorial function
+    __device__ long long int fact(int n)
+    {
+        //Can make this faster by having 3 different functions
+        //fact, 3fact, and 6fact
+        //They will compute and store factorials, so that when we have to compute
+        //the next factorial, it can just take the last factorial and muliply by
+        //n, 3n, or 6n
+        //Leave it for now, as chudnovsky kernel is already fast and this was 
+        //just a fun toy example
+        if (n<2)
+            return 1;
+        return n*fact(n - 1);
+    }
+
+    //atomicAdd function for doubles
+    __device__ double d_atomicAdd(double* address, double val)
+    {
+        //For some reason, I wasn't able to use regular atomicAdd with double values
+        //So I just defined my own function double atomicAdd function to get around it
+        //I'm assuming this is an issue with my own setup, but on the off chance it isn't,
+        //it's best to just define it here
+        unsigned long long int* address_as_ull = (unsigned long long int*)address;
+        unsigned long long int old = *address_as_ull, assumed;
+        do {
+            assumed = old;
+            old = atomicCAS(address_as_ull, assumed,
+                 __double_as_longlong(val + __longlong_as_double(assumed)));
+        } while (assumed != old);
+    return __longlong_as_double(old);
+    }
+
 }
-
-
