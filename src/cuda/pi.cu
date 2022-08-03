e1df15ef809f4cd9cf7d5c1be4b800c5770b7d24
Joseph Wimmergren <105512186+jweswimm@users.noreply.github.com>
2022-08-02 20:39:54 -0400

    Made curand_init a separate kernel
    This change made the function pi_v1() 10x faster!
    Also added a few lines in preparation for pi_v2() (commented out atm)

--- src/cuda/pi.cu
+++ src/cuda/pi.cu
@@ -12,90 +12,130 @@
 
 namespace cuda
 {
-//Initialize necessary variables, create seed, and allocate space on the device
-void pi_init()
-{
-    //Create seed--this can be the same for each thread, see https://ianfinlayson.net/class/cpsc425/notes/cuda-random
-    seed = time(0);
+    __global__ void random_init(unsigned int seed, int samples, curandState* states) {
+        for (int idx = blockIdx.x * blockDim.x + threadIdx.x; idx < samples; idx += gridDim.x * blockDim.x) {
+            curand_init(seed, idx, 0, &states[idx]);
+        }
 
-    //Initialize block size and grid size for the kernel call
-    //For now just do 1D
-    block_size = 1024;
-    grid_size = ceil(double(samples) / double(block_size)); //round up (block_size*grid_size = samples)
-
-    //Allocated space on the device for the number of samples we need
-    cudaMalloc((void**)&d_states, block_size * grid_size * sizeof(curandState));
-
-}
+    }
 
 
-__global__ void est_pi(int* count, unsigned int seed, int samples, curandState* states) {
-    for (int idx = blockIdx.x * blockDim.x + threadIdx.x; idx < samples; idx += gridDim.x * blockDim.x) {
 
-        curand_init(seed, idx, 0, &states[idx]);
+    //Initialize necessary variables, create seed, and allocate space on the device
+    void pi_init_v1()
+    {
+        //Create seed--this can be the same for each thread, see https://ianfinlayson.net/class/cpsc425/notes/cuda-random
+        seed = time(0);
 
-        double x = curand_uniform_double(&states[idx]);
-        double y = curand_uniform_double(&states[idx]);
+        //Initialize block size and grid size for the kernel call
+        //For now just do 1D
+        block_size = 1024;
+        grid_size = ceil(double(samples) / double(block_size)); //round up (block_size*grid_size = samples)
 
-        if (x * x + y * y < 1) { //if within unit circle
+        //Allocated space on the device for the number of samples we need
+        cudaMalloc((void**)&d_states, block_size * grid_size * sizeof(curandState));
 
-            //To avoid race conditions where multiple threads try to
-            //write to the same location at the same time, we use
-            //the atomicAdd operation.
-            atomicAdd((count), 1);
+        random_init << <grid_size, block_size >> > (seed, samples, d_states);
+
+
+    }
+
+
+    __global__ void est_pi(int* count, unsigned int seed, int samples, curandState* states) {
+        for (int idx = blockIdx.x * blockDim.x + threadIdx.x; idx < samples; idx += gridDim.x * blockDim.x) {
+
+//            curand_init(seed, idx, 0, &states[idx]);
+
+            double x = curand_uniform_double(&states[idx]);
+            double y = curand_uniform_double(&states[idx]);
+
+            if (x * x + y * y < 1) { //if within unit circle
+
+                //To avoid race conditions where multiple threads try to
+                //write to the same location at the same time, we use
+                //the atomicAdd operation.
+                atomicAdd((count), 1);
+
+            }
 
         }
 
     }
-    
-}
 
 
 
-//Monte Carlo estimation of pi
-double pi_v1()
-{
-    /*
-    Utility & Explanation: We use a Monte Carlo method to estimate pi.
-    In 2D, we stack a unit circle on a unit square. The area of the
-    unit circle is \pi r^2 and the area of the unit square is 4 r^2.
-    If we randomly generate x and y coordinates for each point, 
-    the ratio of points inside the unit circle to points outside
-    the unit circle and inside the unit square will be an excellent
-    approximation of pi.
-    I.e. (inside circle)/(outside circle) = (\pi r^2) / (4 r^2) = \pi / 4
-    <=> 4*(inside circle)/(outside circle) = \pi
-    Admittedly, this random number generation only generates positive numbers,
-    but because the unit circle and unit square are entirely symmetric across 
-    axes and we are calculating distances from the origin,
-    it doesn't change the estimation.
+    //Monte Carlo estimation of pi
+    double pi_v1()
+    {
+        /*
+        Utility & Explanation: We use a Monte Carlo method to estimate pi.
+        In 2D, we stack a unit circle on a unit square. The area of the
+        unit circle is \pi r^2 and the area of the unit square is 4 r^2.
+        If we randomly generate x and y coordinates for each point,
+        the ratio of points inside the unit circle to points outside
+        the unit circle and inside the unit square will be an excellent
+        approximation of pi.
+        I.e. (inside circle)/(outside circle) = (\pi r^2) / (4 r^2) = \pi / 4
+        <=> 4*(inside circle)/(outside circle) = \pi
+        Admittedly, this random number generation only generates positive numbers,
+        but because the unit circle and unit square are entirely symmetric across
+        axes and we are calculating distances from the origin,
+        it doesn't change the estimation.
 
-    This function returns the estimation of pi.
-    */
+        This function returns the estimation of pi.
+        */
 
-    //Initialize count to 0
-    thrust::device_vector<int> count(1, 0);
+        //Initialize count to 0
+        thrust::device_vector<int> count(1, 0);
 
-    //Call kernel
-    est_pi << <grid_size, block_size >> > (thrust::raw_pointer_cast(count.data()), seed, samples, d_states);
-    gpuErrchk(cudaPeekAtLastError());
-    gpuErrchk(cudaDeviceSynchronize());
+        //Call kernel
+        est_pi << <grid_size, block_size >> > (thrust::raw_pointer_cast(count.data()), seed, samples, d_states);
+        gpuErrchk(cudaPeekAtLastError());
+        gpuErrchk(cudaDeviceSynchronize());
 
-    return 4*double(count[0])/double(samples);
-}
+        return 4 * double(count[0]) / double(samples);
+    }
 
-void pi_reset()
-{
-    /*
-      TODO This function should contain the clean up. You should add
-      memory deallocation etc here.
-    */
+    void pi_reset_v1()
+    {
+        /*
+          TODO This function should contain the clean up. You should add
+          memory deallocation etc here.
+        */
 
-    //Free d_states, as it was on the device
-    cudaFree(d_states);
-    
+        //Free d_states, as it was on the device
+        cudaFree(d_states);
 
-    //Don't need to free thrust device vector count
-}
+
+        //Don't need to free thrust device vector count
+    }
+
+
+
+//    void pi_init_v2() {
+//        //Calculate C
+//        double C = 426880 * sqrt(10005);
+//
+//        //Allocate space for the sum
+//        //Each thread will be one value of the sum
+//        sum_components[]
+//
+//        
+//
+//
+//    }
+//
+//    double pi_v2() {
+//
+//
+//    }
+//
+//    void pi_reset_v2() {
+//
+//    }
+//}
+
+
+
 
 }
